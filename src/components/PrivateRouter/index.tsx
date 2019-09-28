8dc153e7f88e70b46b711b4b4ce1cdd22b93fa55
Washington Alexandre <washington.silva@totvs.com.br>
2019-09-28 01:05:53 -0300

    #route: ajustado rota.

--- src/components/PrivateRouter/index.tsx
+++ src/components/PrivateRouter/index.tsx
@@ -3,7 +3,7 @@ import {  Route, Redirect } from 'react-router-dom';
 
 import { authAuthenticated } from '../../auth';
 
-const PrivateRoute = ({component: Component, ...rest}) => (
+const PrivateRoute = ({component: Component, ...rest}: any) => (
     <Route 
         {...rest}
         render={props => 
